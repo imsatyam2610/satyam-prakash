2f60aef123fc16ce4e2f54c98713ab24c5d96d05
Satyam Prakash <satyam@Satyams-MacBook-Air.local>
2023-10-21 08:26:45 +0530

    Header Error

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,51 +3,6 @@ import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 
 const Header: React.FC = () => {
-  const [typedText, setTypedText] = useState("");
-  const descriptions = [
-    "Full Stack Developer",
-    "UI/UX Designer",
-    "Coder",
-    "Graphic Designer",
-    "Video Editor",
-  ];
-
-  const charIndexRef = useRef(0);
-  const isDeletingRef = useRef(false);
-  const currentDescriptionIndexRef = useRef(0);
-
-  useEffect(() => {
-    const typeDescription = () => {
-      const currentDescription =
-        descriptions[currentDescriptionIndexRef.current];
-      const currentLength = typedText.length;
-
-      if (!isDeletingRef.current) {
-        setTypedText(currentDescription.substring(0, charIndexRef.current + 1));
-        charIndexRef.current++;
-      } else {
-        setTypedText(currentDescription.substring(0, charIndexRef.current - 1));
-        charIndexRef.current--;
-      }
-
-      if (isDeletingRef.current && charIndexRef.current === 0) {
-        isDeletingRef.current = false;
-        currentDescriptionIndexRef.current =
-          (currentDescriptionIndexRef.current + 1) % descriptions.length;
-      }
-
-      if (
-        !isDeletingRef.current &&
-        charIndexRef.current === currentDescription.length
-      ) {
-        isDeletingRef.current = true;
-      }
-    };
-
-    const typeInterval = setInterval(typeDescription, 100);
-    return () => clearInterval(typeInterval);
-  }, []);
-
   return (
     <header className="p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -57,7 +12,7 @@ const Header: React.FC = () => {
           <h1 className="text-5xl font-bold">Satyam Prakash</h1>
           <p className="text-2xl my-6 font-bold">
             <span>I am </span>
-            <span className="text-violet-600">{`${typedText}`}</span>
+            <span className="text-violet-600"></span>
           </p>
           <p>
             I am always looking for new and innovative ways to bring my clients'
