143d935842e0e4f3cb9b83b31aa241581f218897
Satyam Prakash <satyam@Satyams-MacBook-Air.local>
2023-10-21 08:30:53 +0530

    Header ESLint Error

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,13 +15,13 @@ const Header: React.FC = () => {
             <span className="text-violet-600"></span>
           </p>
           <p>
-            I am always looking for new and innovative ways to bring my clients'
+            I am always looking for new and innovative ways to bring my clients
             visions to life. I believe that a webpage is about more than just
-            making things look pretty - it's about solving problems and creating
+            making things look pretty - its about solving problems and creating
             intuitive, enjoyable experiences for users.
           </p>
           <p className="my-5">
-            Whether I'm working on a website, mobile app, or other digital
+            Whether Im working on a website, mobile app, or other digital
             product, I bring my commitment to design excellence and
             user-centered thinking to every project I work on. I look forward to
             the opportunity to bring my skills and passion to your next project.
